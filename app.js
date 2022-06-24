38fb85276c03c2ff66ddaf4ab68b89ab6a364f2e
Steven Abuan <stevenjosephabuan@gmail.com>
2022-06-24 17:57:01 +0800

    second commit

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(expressLayouts);
 app.use(express.static("public"));
 app.use("/", indexRouter);
 
-mongoose.connect(process.env.DATA_URL);
+mongoose.connect(process.env.DATABASE_URL);
 const db = mongoose.connection;
 db.on("error", (error) => {
   console.error(error);
